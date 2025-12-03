2a861ef5c9e388a4a0ec746652da51f86ff6936a
Aleena Khan <toutetrakhan@gmail.com>
2025-12-02 23:29:03 -0800

    Clean up blank lines in helpers.h
    Removed unnecessary blank lines in helpers.h

--- helpers.h
+++ helpers.h
@@ -1,19 +1,13 @@
 #ifndef HELPERS_H
 #define HELPERS_H
-
 #include <string>
 #include <cstdint>
 using namespace std;
 
 // Convert a string to a number
 uint32_t parseNumber(const string &inputString);
-
-
 // Convert an integer to a hexadecimal string
 string toHex(uint32_t number);
-
-
 // Remove leading and trailing whitespace from a string
 string trim(const string &inputString);
-
 #endif
